0207f8d712cc36b0dfb587e2f4aef88e1173e3a2
Artur Nowak <arturmareknowal@gmail.com>
2022-05-09 20:47:44 +0200

    implemented first version of tcp client - server - client solution

---
+++ TCP/TcpClientOne/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class TcpClientOne
+{
+    public static void Main()
+    {
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        var ipEndpoint = new IPEndPoint(ipAddress, 12346);
+        var tcpClient = new TcpClient(ipEndpoint);
+        
+        tcpClient.Connect(ipAddress, 12345);
+        
+        Byte[] data = System.Text.Encoding.ASCII.GetBytes("message");
+        
+        NetworkStream stream = tcpClient.GetStream();
+        
+        stream.Write(data, 0, data.Length);
+
+        Console.WriteLine("Sent: {0}", "message");
+        
+        data = new Byte[256];
+
+        String responseData = String.Empty;
+
+        Int32 bytes = stream.Read(data, 0, data.Length);
+        responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+        Console.WriteLine("Received: {0}", responseData);
+
+        // Close everything.
+        stream.Close();
+        tcpClient.Close();
+    }
+}
---
+++ TCP/TcpServer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class TcpServer
+{
+    public static void Main()
+    {
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        var ipEndpoint = new IPEndPoint(ipAddress, 12345);
+        var tcpServer = new TcpListener(ipEndpoint);
+        
+        tcpServer.Start();
+        
+        Console.Write("Waiting for a connection... ");
+        
+        TcpClient client = tcpServer.AcceptTcpClient();            
+        Console.WriteLine("Connected!");
+        
+        TaskFactory taskFactory = new TaskFactory();
+        var taskArray = new Task[2];
+        taskArray[0] = taskFactory.StartNew(() => ReadingThread(client));
+        taskArray[1] = taskFactory.StartNew(() => SendingThread(client));
+
+        Task.WaitAll(taskArray);
+        Console.WriteLine("Here");
+    }
+
+    private static void ReadingThread(TcpClient client)
+    {
+        Byte[] bytes = new Byte[256];
+        String data = null;
+        
+        NetworkStream stream = client.GetStream();
+
+        int i;
+
+        // Loop to receive all the data sent by the client.
+        while((i = stream.Read(bytes, 0, bytes.Length))!=0)
+        {
+            data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+            Console.WriteLine("Received: {0}", data);
+        }
+    }
+
+    private static void SendingThread(TcpClient client)
+    {
+        NetworkStream stream = client.GetStream();
+        
+        var data = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes("foo"), 0, 3);
+        byte[] msg = Encoding.ASCII.GetBytes(data);
+        stream.Write(msg, 0, msg.Length);
+    }
+}
