df8207f4f58bfca8387e66ef4339c99ef921c6f0
Artur Nowak <arturmareknowal@gmail.com>
2022-05-13 20:04:36 +0200

    added cancellation token to server threads and added task factory to client one

--- TCP/TcpClientOne/Program.cs
+++ TCP/TcpClientOne/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 class TcpClientOne
 {
-    public static void Main()
+    public static async Task Main()
     {
         IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
         var ipEndpoint = new IPEndPoint(ipAddress, 12346);
@@ -21,7 +21,22 @@ class TcpClientOne
         
         NetworkStream stream = tcpClient.GetStream();
         
-        stream.Write(data, 0, data.Length);
+        TaskFactory taskFactory = new TaskFactory();
+        var tokenSource = new CancellationTokenSource();
+        CancellationToken cancellationToken = tokenSource.Token;
+        
+        var taskArray = new Task[2];
+        taskArray[0] = taskFactory.StartNew(() => ReadingThread(tcpClient, cancellationToken));
+        taskArray[1] = taskFactory.StartNew(() => SendingThread(tcpClient, cancellationToken));
+        
+        while (!(Console.ReadLine() == "q"))
+        {
+            tokenSource.Cancel();
+        }
+        
+        Task.WaitAll(taskArray);
+        
+        /*stream.Write(data, 0, data.Length);
 
         Console.WriteLine("Sent: {0}", "message");
         
@@ -32,9 +47,43 @@ class TcpClientOne
         Int32 bytes = stream.Read(data, 0, data.Length);
         responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
         Console.WriteLine("Received: {0}", responseData);
+        */
 
         // Close everything.
         stream.Close();
         tcpClient.Close();
+    }
+    
+    private static void ReadingThread(TcpClient client, CancellationToken cancellationToken)
+    {
+        Byte[] bytes = new Byte[256];
+        String data = null;
+        
+        NetworkStream stream = client.GetStream();
+
+        int i;
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            // Loop to receive all the data sent by the client.
+            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                Console.WriteLine("Received: {0}", data);
+            }
+        }
+    }
+
+    private static async Task SendingThread(TcpClient client, CancellationToken cancellationToken)
+    {
+        NetworkStream stream = client.GetStream();
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            var data = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes("bar"), 0, 3);
+            byte[] msg = Encoding.ASCII.GetBytes(data);
+            stream.Write(msg, 0, msg.Length);
+            await Task.Delay(1500);
+        }
     }
 }
--- TCP/TcpServer/Program.cs
+++ TCP/TcpServer/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 class TcpServer
@@ -20,15 +21,25 @@ class TcpServer
         Console.WriteLine("Connected!");
         
         TaskFactory taskFactory = new TaskFactory();
+        var tokenSource = new CancellationTokenSource();
+        CancellationToken cancellationToken = tokenSource.Token;
+        
         var taskArray = new Task[2];
-        taskArray[0] = taskFactory.StartNew(() => ReadingThread(client));
-        taskArray[1] = taskFactory.StartNew(() => SendingThread(client));
+        taskArray[0] = taskFactory.StartNew(() => ReadingThread(client, cancellationToken));
+        taskArray[1] = taskFactory.StartNew(() => SendingThread(client, cancellationToken));
 
+        Console.WriteLine("HereHere");
+        
+        while (!(Console.ReadLine() == "q"))
+        {
+            tokenSource.Cancel();
+        }
+        
         Task.WaitAll(taskArray);
         Console.WriteLine("Here");
     }
 
-    private static void ReadingThread(TcpClient client)
+    private static void ReadingThread(TcpClient client, CancellationToken cancellationToken)
     {
         Byte[] bytes = new Byte[256];
         String data = null;
@@ -37,20 +48,27 @@ class TcpServer
 
         int i;
 
-        // Loop to receive all the data sent by the client.
-        while((i = stream.Read(bytes, 0, bytes.Length))!=0)
+        while (!cancellationToken.IsCancellationRequested)
         {
-            data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-            Console.WriteLine("Received: {0}", data);
+            // Loop to receive all the data sent by the client.
+            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                Console.WriteLine("Received: {0}", data);
+            }
         }
     }
 
-    private static void SendingThread(TcpClient client)
+    private static async Task SendingThread(TcpClient client, CancellationToken cancellationToken)
     {
         NetworkStream stream = client.GetStream();
-        
-        var data = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes("foo"), 0, 3);
-        byte[] msg = Encoding.ASCII.GetBytes(data);
-        stream.Write(msg, 0, msg.Length);
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            var data = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes("foo"), 0, 3);
+            byte[] msg = Encoding.ASCII.GetBytes(data);
+            stream.Write(msg, 0, msg.Length);
+            await Task.Delay(3000);
+        }
     }
 }
