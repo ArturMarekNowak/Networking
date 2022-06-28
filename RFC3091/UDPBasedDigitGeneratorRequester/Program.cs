7be3224034ef4a18725d07e3ca3ab8cf1817eae1
Artur Nowak <arturmareknowal@gmail.com>
2022-06-28 13:09:59 +0200

    implemented RFC3091

--- RFC3091/UDPBasedDigitGeneratorRequester/Program.cs
+++ RFC3091/UDPBasedDigitGeneratorRequester/Program.cs
@@ -1,3 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-Console.WriteLine("Hello, World!");
+namespace RFC3091
+{
+    public sealed class Program
+    {
+        public static void Main()
+        {
+            var clientEndpoint = new IPEndPoint(IPAddress.Loopback, 12348);
+            var basedDigitGeneratorEndPoint = new IPEndPoint(IPAddress.Loopback, 31415);
+            var udpClient = new UdpClient(clientEndpoint);
+            
+            var tokenSource = new CancellationTokenSource();
+            
+            Task.WaitAny(new []
+            {
+                Task.Run(() => ReadIncomingMessages(udpClient, basedDigitGeneratorEndPoint, tokenSource)),
+                Task.Run(() => SendUserInputs(udpClient, basedDigitGeneratorEndPoint, tokenSource))
+            });
+            
+            udpClient.Close();
+        }
+
+        private static void ReadIncomingMessages(UdpClient udpClient, IPEndPoint ipEndPoint, CancellationTokenSource tokenSource)
+        {
+            while (!tokenSource.Token.IsCancellationRequested)
+            {
+                var readBytes = udpClient.Receive(ref ipEndPoint);
+                var message= Encoding.ASCII.GetString(readBytes, 0, readBytes.Length);
+                Console.WriteLine($"{DateTime.Now}: {message}");
+            }
+            
+            Console.WriteLine("Closing application...");
+        }
+
+        private static void SendUserInputs(UdpClient udpClient, IPEndPoint ipEndPoint, CancellationTokenSource tokenSource)
+        {
+            do
+            {
+                var userInput = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(userInput))
+                {
+                    Console.WriteLine("Input must be an integer number!!!");
+                    continue;
+                }
+
+                if (userInput.Equals("q"))
+                {
+                    Console.WriteLine("Closing application...");
+                    tokenSource.Cancel();
+                    break;
+                }
+                
+                if (!int.TryParse(userInput, out var number))
+                {
+                    Console.WriteLine("Input must be an integer number!!!");
+                    continue;
+                }
+
+                var bytes = Encoding.ASCII.GetBytes(userInput);
+                udpClient.Send(bytes, bytes.Length, ipEndPoint);
+                
+            } while (!tokenSource.Token.IsCancellationRequested);
+        }
+    }
+}
